14acb8229f4818c035aaa7811c2ca288f00d2e36
Ali Burhan <aliburhan42000@gmail.com>
2024-01-13 16:18:55 +0500

    Correcting the comma production error

--- src/app/components/Herosection.js
+++ src/app/components/Herosection.js
@@ -5,7 +5,7 @@ const Herosection = () => {
         <div className="grid grid-cols-1 sm:grid-cols-12">
             <div className=" col-span-7 place-self-center text-center sm:text-left">
         <h1 className='text-white mb-4 text-4xl sm:text-5xl lg:text-6xl font-extrabold'>
-<div className="text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">Hello, I'm {" "}</div>
+<div className="text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">Hello, I&apos;m {" "}</div>
 Ali Burhan
       </h1>
       <p className="text-[#ADB7BE] sm:text-lg lg:text-xl mb-6 ">
